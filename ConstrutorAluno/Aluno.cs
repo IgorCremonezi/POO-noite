10a41b7901fae133001f20fc7d621e830ab2e1b4
IgorCremonezi <igc2009_guilherme@hotmail.com>
2024-04-08 21:08:24 -0300

    08042024

--- ConstrutorAluno/Aluno.cs
+++ ConstrutorAluno/Aluno.cs
@@ -7,20 +7,65 @@ namespace ConstrutorAluno
 {
     public class Aluno
     {
-        //.*.*.*.* TRABALHO, VALE 1 PONTO *.*.*.*.
+        private long matricula;
+        public long Matricula
+        {
+            get { return matricula; }
+            set { matricula = value; }
+        }
+        private static int contador;
+        private string nome;
+        public string Nome
+        {
+            get { return nome; }
+            set
+            {
+                if (value != "")
+                    nome = value;
+                else
+                    System.Console.WriteLine("\nDigite um nome válido!");
+            }
+        }
+        public double P1 { get; set; }
+        public double P2 { get; set; }
+        public double Media { get; private set; }
 
-        //Data de entrega 31/03, pelo Teams, arquivo em .ZIP
+        static Aluno()
+        {
+            contador = 1;
+        }
+        public Aluno()
+        {
+            Matricula = 1570482211000 + contador;
+            contador++;
+        }
 
-        //Criar os atributos matrícula, nome, p1, p2, media
-        //Criar os métodos/propriedade get e set, exceto o
-        //atributo média
-        //Criar o método para calcular a média do aluno
-        //O atributo matrícula não terá o método set, pois
-        //será gerado por um atributo static e armazenado
-        //no atributo matricula 
-        //Não use vetor
-        //Crie o método MostrarAtributos, chame-o após cada
-        //instância
-        //Instancia no mínimo 4 objetos
+        public Aluno(string nome, double p1, double p2)
+        {
+            Nome = nome;
+            P1 = p1;
+            P2 = p2;
+            this.CalcularMediaInterna();
+            Matricula = 1570482211000 + contador;
+            contador++;
+        }
+        public void CalcularMediaInterna()
+        {
+            Media = (P1 + P2) / 2;
+        }
+
+        public void CalcularMediaMain(double p1, double p2)
+        {
+            Media = (p1 + p2) / 2;
+        }
+
+        public void MostrarAtributos()
+        {
+            System.Console.WriteLine("\nMatricula do aluno: " + Matricula);
+            System.Console.WriteLine("Nome do aluno: " + Nome);
+            System.Console.WriteLine("Nota da P1: " + P1);
+            System.Console.WriteLine("Nota da P2: " + P2);
+            System.Console.WriteLine("Média das notas: " + Media);
+        }
     }
 }
