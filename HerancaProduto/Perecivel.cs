351bd9e733d6437bf72a30c366efb552809cf092
IgorCremonezi <igc2009_guilherme@hotmail.com>
2024-04-29 20:35:17 -0300

    aula29042024

---
+++ HerancaProduto/Perecivel.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HerancaProduto
+{
+    public class Perecivel : Produto
+    {
+        private string dataFabricacao;
+        public string DataFabricacao
+        {
+            get { return dataFabricacao; }
+            set { dataFabricacao = value; }
+        }
+        private string dataValidade;
+        public string DataValidade
+        {
+            get { return dataValidade; }
+            set { dataValidade = value; }
+        }
+        private int lote;
+        public int Lote
+        {
+            get { return lote; }
+            set { lote = value; }
+        }
+        public Perecivel(int codigo, string nome, double preco, string dataFabricacao, string dataValidade, int lote) : base (codigo, nome, preco)
+        {
+            DataFabricacao = dataFabricacao;
+            DataValidade = dataValidade;
+            Lote = lote;
+        }
+
+        public Perecivel() : base()
+        {
+        }
+
+        public void MostrarAtributos()
+        {
+            base.MostrarAtributos();
+            System.Console.WriteLine("Data de fabricação do produto: " + DataFabricacao);
+            System.Console.WriteLine("Data de validade do produto: " + DataValidade);
+            System.Console.WriteLine("Número do lote do produto: " + Lote);
+        }
+    }
+}
---
+++ HerancaProduto/Program.cs
@@ -0,0 +1,9 @@
+﻿using HerancaProduto;
+
+Produto produto = new Produto();
+Produto p1 = new Produto(1, "mouse", 250);
+p1.MostrarAtributos();
+
+Perecivel perecivel = new Perecivel();
+Perecivel pe2 = new Perecivel(2, "lanche", 20, "28/04/2024", "30/04/2024", 24);
+pe2.MostrarAtributos();
