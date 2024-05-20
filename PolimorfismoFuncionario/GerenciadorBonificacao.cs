eeef6edfbe25cced76b09c435eec250d75763818
IgorCremonezi <igc2009_guilherme@hotmail.com>
2024-05-20 19:18:58 -0300

    trabalhopolimorfismo

---
+++ PolimorfismoFuncionario/GerenciadorBonificacao.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PolimorfismoFuncionario
+{
+    public class GerenciadorBonificacao
+    {
+        private double totalBonificacao;
+        public double TotalBonificacao
+        {
+            get { return totalBonificacao; }
+            set { totalBonificacao = value; }
+        }
+        
+        public void TotalizadorBonificacao(Funcionario funcionario)
+        {
+            this.TotalBonificacao += funcionario.CalcularBonificacao();
+        }
+
+        public void TotalizadorBonificacao(Diretor diretor)
+        {
+            this.TotalBonificacao += diretor.CalcularBonificacao();
+        }
+
+        public void TotalizadorBonificacao(Gerente gerente)
+        {
+            this.TotalBonificacao += gerente.CalcularBonificacao();
+        }
+
+        public void TotalizadorBonificacao(Secretario secretario)
+        {
+            this.TotalBonificacao += secretario.CalcularBonificacao();
+        }
+    }
+}
--- PolimorfismoFuncionario/Program.cs
+++ PolimorfismoFuncionario/Program.cs
@@ -11,3 +11,11 @@ System.Console.WriteLine("\nBonificação do gerente: " + g.CalcularBonificacao(
 
 Diretor d = new Diretor(4, "Igor", 1000);
 System.Console.WriteLine("\nBonificação do diretor: " + d.CalcularBonificacao());
+
+GerenciadorBonificacao gb = new GerenciadorBonificacao();
+gb.TotalizadorBonificacao(f);
+gb.TotalizadorBonificacao(s);
+gb.TotalizadorBonificacao(g);
+gb.TotalizadorBonificacao(d);
+
+System.Console.WriteLine("\nTotal da bonificação: " + gb.TotalBonificacao);
